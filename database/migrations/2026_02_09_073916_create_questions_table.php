afb62d090a344a9770cd5c5cab41b6007dbd4b6f
{Christian López} <chlopez89@gmail.com>
2026-02-17 00:27:28 -0600

    Modificacion de la migracion

--- database/migrations/2026_02_09_073916_create_questions_table.php
+++ database/migrations/2026_02_09_073916_create_questions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
              
-            $table->string('tittle');
+            $table->string('title');
             $table->text('description');
 
             $table->timestamps();
